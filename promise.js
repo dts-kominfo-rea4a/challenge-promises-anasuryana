dbf9f28de5c0d8c73e071bfe036ea1372ada63ea
anasuryana <anasuryana10@gmail.com>
2022-09-23 20:10:15 +0700

    komit 1 tugas 2

--- promise.js
+++ promise.js
@@ -1,8 +1,30 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (param) => {
+  let tmpdata = 0
+  await promiseTheaterIXX()
+  .then(async (resolveValue) => {
+    resolveValue.forEach((data, index) => {
+      if(data.hasil===param)
+      {
+        tmpdata++
+      }
+    })
+    await promiseTheaterVGC()
+    .then((resolveValue) => {
+      resolveValue.forEach((data, index) => {
+        if(data.hasil===param)
+        {
+          tmpdata++
+        }
+      })
+    })
+  })
+  return tmpdata
+};
 
 module.exports = {
   promiseOutput,
 };
+ 
